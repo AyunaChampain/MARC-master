35c0a76491a3f0065bd1aa013daaa2fa9686f4cf
AyunaChampain <151037671+AyunaChampain@users.noreply.github.com>
2024-10-24 08:27:54 +0200

    Change 1

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
  #include <stdio.h>
 #include "map.h"
 
+//Github test
+
 int main() {
     t_map map = createMapFromFile("..\\maps\\example1.map");
     printf("Map created with dimensions %d x %d\n", map.y_max, map.x_max);
