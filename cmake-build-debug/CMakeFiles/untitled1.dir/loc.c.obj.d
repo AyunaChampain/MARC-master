4c3a11338a959a8309ca1465eb252284b6e2e865
JuliaTea <julia.tea@efrei.net>
2024-10-24 09:10:24 +0200

    Commit message

---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/loc.c.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/untitled1.dir/loc.c.obj: \
+ C:\Users\julia\CLionProjects\MARC-master\loc.c \
+ C:\Users\julia\CLionProjects\MARC-master\loc.h
---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/moves.c.obj.d
@@ -0,0 +1,4 @@
+CMakeFiles/untitled1.dir/moves.c.obj: \
+ C:\Users\julia\CLionProjects\MARC-master\moves.c \
+ C:\Users\julia\CLionProjects\MARC-master\moves.h \
+ C:\Users\julia\CLionProjects\MARC-master\loc.h
